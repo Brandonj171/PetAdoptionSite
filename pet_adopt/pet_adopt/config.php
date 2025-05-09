5873e73fcdb67bca3330ffce39627a8c6028ee8c
Brandonj171 <Brandon.jordan14@outlook.com>
2025-05-09 11:20:44 -0400

    Delete pet_adopt directory

--- pet_adopt/pet_adopt/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Database credentials
-define('DBHOST', '3.131.52.57');
-define('DBNAME', 'pet_adopt');
-define('DBUSER', 'user1');      // replace with your MySQL username
-define('DBPASS', 'user1pwd!');      // replace with your MySQL password
-
-define('DBCONNSTRING', "mysql:host=" . DBHOST . ";dbname=" . DBNAME . ";charset=utf8mb4");
