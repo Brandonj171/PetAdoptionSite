d6e79c274b39042fb9534cdb7b79f754eda1a3b2
Brandonj171 <Brandon.jordan14@outlook.com>
2025-05-10 22:14:53 -0400

    Update chatbot.php

--- pet_adopt/chatbot.php
+++ pet_adopt/chatbot.php
@@ -2,7 +2,7 @@
 <div class="paws-chat-wrapper">
   <img src="images/paws-dog.png" alt="Ask Paws" id="paws-icon" />
   <div class="chatbot-container" id="chat-container">
-    <div class="chat-header">🐶 Ask Paws</div>
+    <div class="chat-header"> Ask Paws</div>
     <div class="chat-box" id="chat-box">
       <div class="bot-message">Woof! Ask me anything about adoption, volunteering, or more!</div>
     </div>
