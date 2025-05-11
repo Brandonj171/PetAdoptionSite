91a3b041b5b8912470264fbc72383b5cc9a15035
Brandonj171 <Brandon.jordan14@outlook.com>
2025-05-10 21:31:43 -0400

    Create chatbot.php

---
+++ pet_adopt/chatbot.php
@@ -0,0 +1,13 @@
+<div class="chatbot-container">
+  <div class="chat-header">
+    🐾 AskPaws
+    <button id="chat-toggle">−</button>
+  </div>
+  <div class="chat-box" id="chat-box">
+    <div class="bot-message">Select a question below:</div>
+    <div class="faq-options" id="faq-options"></div>
+  </div>
+</div>
+
+<link rel="stylesheet" href="css/chatbot.css">
+<script src="js/chatbot.js" defer></script>
