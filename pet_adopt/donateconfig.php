f2f325cf6df52f8e1c5abe0e2415f89a7589b2bc
Brandonj171 <Brandon.jordan14@outlook.com>
2025-05-18 18:22:06 -0400

    Delete pet_adopt directory

--- pet_adopt/donateconfig.php
+++
@@ -1,7 +0,0 @@
-<?php
-define('DBHOST', 'localhost');
-define('DBNAME', 'petadopt_db');
-define('DBUSER', 'testuser');
-define('DBPASS', 'mypassword');
-define('DBCONNSTRING','mysql:dbname=petadopt_db;charset=utf8mb4;');
-?>
